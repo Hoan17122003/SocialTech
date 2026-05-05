4be4d42b75762edcbaca82f7a6adafb9861ec947
hoan17122003 <hoanpice32@gmail.com>
2026-05-05 21:44:16 +0700

    remove all folder debug when ASP.NET compiler

--- SocialBackEnd/obj/Debug/net8.0/SocialBackEnd.AssemblyInfo.cs
+++ SocialBackEnd/obj/Debug/net8.0/SocialBackEnd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SocialBackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b17bfaf3220aa7e740171279fc74f1d20777f352")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e94eaa8443cd5bf1c620018cfa9e9b279897d20")]
 [assembly: System.Reflection.AssemblyProductAttribute("SocialBackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SocialBackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
